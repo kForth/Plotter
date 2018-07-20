ab854d41047063c09cdfc2ab1a2e62ad777da9b0
Kestin Goforth <kgoforth1503@gmail.com>
2018-07-20 12:39:48 -0400

    update freeze script

--- scripts/freeze.sh
+++ scripts/freeze.sh
@@ -1,2 +1,17 @@
+echo "Clearing dist"
 rm -r dist/*
+echo "Freezing with PyInstaller"
 pyinstaller --windowed Plotter.spec --hidden-import PyQt5.sip
+# echo "Creating temp folder"
+# mkdir "dist/Plotter_dmg"
+# cp -r "dist/Plotter.app" "dist/Plotter_dmg/Plotter.app"
+# ln -s "/Applications" "dist/Plotter_dmg/Applications"
+# echo "Creating dmg"
+# hdiutil create -volname "Plotter" -srcfolder "dist/Plotter_dmg" -ov -format UDZO "dist/Plotter.dmg"
+# echo "Removing temp folder"
+# rm -r "dist/Plotter_dmg"
+echo "Zipping app"
+cd dist
+zip Plotter_For_Mac.zip Plotter.app
+cd ..
+echo "Done"
